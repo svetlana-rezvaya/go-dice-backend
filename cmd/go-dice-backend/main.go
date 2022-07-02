e7d3c41159d6fe1687c8ec455ec208d625135125
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-07-02 21:52:01 +0300

    Present the result in JSON

--- cmd/go-dice-backend/main.go
+++ cmd/go-dice-backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,11 @@ import (
 	"github.com/svetlana-rezvaya/go-dice-cli"
 	"github.com/svetlana-rezvaya/go-dice-cli/statistics"
 )
+
+type result struct {
+	Throws     []int
+	Statistics statistics.Statistics
+}
 
 func main() {
 	http.HandleFunc("/api/v1/dice", func(writer http.ResponseWriter, request *http.Request) {
@@ -51,7 +57,18 @@ func main() {
 		throwResults := dice.GenerateDiceThrows(throws, faces)
 		throwStatistics := statistics.CollectStatistics(throwResults)
 
-		fmt.Fprintf(writer, "throwResults: %v\nthrowStatistics: %+v", throwResults, throwStatistics)
+		throwTotalResult := result{Throws: throwResults, Statistics: throwStatistics}
+		responseBytes, err := json.Marshal(throwTotalResult)
+		if err != nil {
+			errMessage := fmt.Sprintf("unable to marshal the response: %s", err)
+			log.Print(errMessage)
+			http.Error(writer, errMessage, http.StatusInternalServerError)
+
+			return
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
+		writer.Write(responseBytes)
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
