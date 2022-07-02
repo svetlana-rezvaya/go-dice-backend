0e2069303f4966117a46d05c660e6367777502ef
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-07-02 21:16:12 +0300

    Parse the request parameters

--- cmd/go-dice-backend/main.go
+++ cmd/go-dice-backend/main.go
@@ -4,11 +4,48 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, world!")
+	http.HandleFunc("/api/v1/dice", func(writer http.ResponseWriter, request *http.Request) {
+		throwsAsString := request.FormValue("throws")
+		if throwsAsString == "" {
+			const errMessage = "throw count is missing"
+			log.Print(errMessage)
+			http.Error(writer, errMessage, http.StatusBadRequest)
+
+			return
+		}
+
+		throws, err := strconv.Atoi(throwsAsString)
+		if err != nil {
+			errMessage := fmt.Sprintf("incorrect throw count: %s", err)
+			log.Print(errMessage)
+			http.Error(writer, errMessage, http.StatusBadRequest)
+
+			return
+		}
+
+		facesAsString := request.FormValue("faces")
+		if facesAsString == "" {
+			const errMessage = "face count is missing"
+			log.Print(errMessage)
+			http.Error(writer, errMessage, http.StatusBadRequest)
+
+			return
+		}
+
+		faces, err := strconv.Atoi(facesAsString)
+		if err != nil {
+			errMessage := fmt.Sprintf("incorrect face count: %s", err)
+			log.Print(errMessage)
+			http.Error(writer, errMessage, http.StatusBadRequest)
+
+			return
+		}
+
+		fmt.Fprintf(writer, "throws: %d\nfaces: %d", throws, faces)
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
