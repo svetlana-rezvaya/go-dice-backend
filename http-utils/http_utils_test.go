690e6f996506d649013d08a8a9ea61190c60d4df
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-07-11 20:36:43 +0300

    Add the tests for the HandleError function

--- http-utils/http_utils_test.go
+++ http-utils/http_utils_test.go
@@ -1,8 +1,14 @@
 package httputils
 
 import (
+	"bytes"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -69,6 +75,82 @@ func TestGetIntFormValue(test *testing.T) {
 				testData.name,
 				testData.wantedErrStr,
 				err,
+			)
+			test.Fail()
+		}
+	}
+}
+
+func TestHandleError(test *testing.T) {
+	type args struct {
+		status    int
+		format    string
+		arguments []interface{}
+	}
+
+	tests := []struct {
+		name             string
+		args             args
+		wantedLogMessage string
+		wantedResponse   *http.Response
+	}{
+		{
+			name: "succes",
+			args: args{
+				status:    http.StatusNotFound,
+				format:    "test: %d %s",
+				arguments: []interface{}{23, "one"},
+			},
+			wantedLogMessage: "test: 23 one\n",
+			wantedResponse: &http.Response{
+				Status: strconv.Itoa(http.StatusNotFound) + " " +
+					http.StatusText(http.StatusNotFound),
+				StatusCode: http.StatusNotFound,
+				Proto:      "HTTP/1.1",
+				ProtoMajor: 1,
+				ProtoMinor: 1,
+				Header: http.Header{
+					"Content-Type":           []string{"text/plain; charset=utf-8"},
+					"X-Content-Type-Options": []string{"nosniff"},
+				},
+				Body:          ioutil.NopCloser(bytes.NewReader([]byte("test: 23 one\n"))),
+				ContentLength: -1,
+			},
+		},
+	}
+	for _, testData := range tests {
+		previousLogOutput := log.Writer()
+		defer func() { log.SetOutput(previousLogOutput) }()
+
+		var logBuffer bytes.Buffer
+		log.SetOutput(&logBuffer)
+
+		responseRecorder := httptest.NewRecorder()
+		HandleError(
+			responseRecorder,
+			testData.args.status,
+			testData.args.format,
+			testData.args.arguments...,
+		)
+
+		logMessage := logBuffer.String()
+		if !strings.HasSuffix(logMessage, testData.wantedLogMessage) {
+			test.Logf(
+				"failed %q:\n  expected: %+v\n  actual: %+v",
+				testData.name,
+				testData.wantedLogMessage,
+				logMessage,
+			)
+			test.Fail()
+		}
+
+		response := responseRecorder.Result()
+		if !reflect.DeepEqual(response, testData.wantedResponse) {
+			test.Logf(
+				"failed %q:\n  expected: %+v\n  actual: %+v",
+				testData.name,
+				testData.wantedResponse,
+				response,
 			)
 			test.Fail()
 		}
