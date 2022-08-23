416da6335155fc5560bc4b2f3fb20b63f4f84180
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-08-23 23:00:58 +0300

    Set up checks in load testing

--- tests/load_tests.js
+++ tests/load_tests.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import { check, sleep } from "k6";
 
 export const options = {
   stages: [
@@ -11,10 +11,16 @@ export const options = {
     http_req_failed: ["rate < 0.01"], // HTTP errors should be less than 1%
     // 90% of requests must finish within 400ms, 95% within 800, and 99.9% within 2s.
     http_req_duration: ["p(90) < 400", "p(95) < 800", "p(99.9) < 2000"],
+    // the rate of successful checks should be higher than 90%
+    checks: ["rate > 0.9"],
   },
 };
 
 export default function () {
-  http.get("https://test.k6.io");
+  const response = http.get("http://test.k6.io/");
+  check(response, {
+    "is status 200": (response) => response.status === 200,
+  });
+
   sleep(1);
 }
