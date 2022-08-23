00b3198eceafae5a8fb630b0784f3838ccfa4f4f
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-08-23 22:21:49 +0300

    Set up ramping in load testing

--- tests/load_tests.js
+++ tests/load_tests.js
@@ -2,8 +2,11 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export const options = {
-  vus: 10,
-  iterations: 100,
+  stages: [
+    { duration: "5s", target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 seconds
+    { duration: "10s", target: 100 }, // stay at 100 users for 10 seconds
+    { duration: "5s", target: 0 }, // ramp-down to 0 users over 5 seconds
+  ],
 };
 
 export default function () {
