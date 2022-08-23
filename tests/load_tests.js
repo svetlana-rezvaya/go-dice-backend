eb5bfe9bc38c0660a3683410306d9ea1d0c88cb5
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-08-23 22:49:03 +0300

    Set up thresholds in load testing

--- tests/load_tests.js
+++ tests/load_tests.js
@@ -7,6 +7,11 @@ export const options = {
     { duration: "10s", target: 100 }, // stay at 100 users for 10 seconds
     { duration: "5s", target: 0 }, // ramp-down to 0 users over 5 seconds
   ],
+  thresholds: {
+    http_req_failed: ["rate < 0.01"], // HTTP errors should be less than 1%
+    // 90% of requests must finish within 400ms, 95% within 800, and 99.9% within 2s.
+    http_req_duration: ["p(90) < 400", "p(95) < 800", "p(99.9) < 2000"],
+  },
 };
 
 export default function () {
