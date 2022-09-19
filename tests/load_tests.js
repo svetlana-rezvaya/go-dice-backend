278a4b4893d207bb5d1642c6cd1d6b594384e075
svetlana-rezvaya <svetlana-2305@mail.ru>
2022-09-19 21:55:32 +0300

    Improve the code style of the load test

--- tests/load_tests.js
+++ tests/load_tests.js
@@ -1,26 +1,39 @@
+import { expect } from "https://jslib.k6.io/k6chaijs/4.3.4.1/index.js";
+import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.4.0/index.js";
 import http from "k6/http";
 import { sleep } from "k6";
-import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.4.0/index.js";
-import { expect } from "https://jslib.k6.io/k6chaijs/4.3.4.1/index.js";
 
 export const options = {
   stages: [
-    { duration: "5s", target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 seconds
-    { duration: "10s", target: 100 }, // stay at 100 users for 10 seconds
-    { duration: "5s", target: 0 }, // ramp-down to 0 users over 5 seconds
+    // simulate ramp-up of traffic from 1 to 100 users over 5 seconds
+    { duration: "5s", target: 100 },
+    // stay at 100 users for 10 seconds
+    { duration: "10s", target: 100 },
+    // ramp-down to 0 users over 5 seconds
+    { duration: "5s", target: 0 },
   ],
   thresholds: {
-    http_req_failed: ["rate < 0.01"], // HTTP errors should be less than 1%
-    // 90% of requests must finish within 400ms, 95% within 800, and 99.9% within 2s.
+    // the rate of HTTP errors should be less than 1%
+    http_req_failed: ["rate < 0.01"],
+    // 90% of requests must finish within 400ms, 95% within 800, and 99.9% within 2s
     http_req_duration: ["p(90) < 400", "p(95) < 800", "p(99.9) < 2000"],
     // the rate of successful checks should be higher than 90%
     checks: ["rate > 0.9"],
   },
 };
 
+function expectBody(response) {
+  return expect(response.json(), "response body");
+}
+
+function expectBodyProperty(response, property) {
+  return expect(response.json(property), `property '${property}'`);
+}
+
 export default function () {
   const throws = randomIntBetween(1, 100);
   const faces = randomIntBetween(2, 100);
+
   const response = http.get(
     `http://${__ENV.SERVICE_ADDRESS}/api/v1/dice?throws=${throws}&faces=${faces}`
   );
@@ -28,32 +41,25 @@ export default function () {
   expect(response.status, "status code").to.equal(200);
   expect(response).to.have.validJsonBody();
 
-  expect(response.json(), "response body").to.have.property("Throws");
-  expect(response.json("Throws"), "property 'Throws'")
+  expectBody(response).to.have.property("Throws");
+  expectBodyProperty(response, "Throws")
     .to.be.an("array")
     .to.have.lengthOf(throws);
 
-  expect(response.json(), "response body").to.have.property("Statistics");
-  expect(response.json("Statistics"), "property 'Statistics'").to.have.property(
-    "Minimum"
-  );
-  expect(
-    response.json("Statistics.Minimum"),
-    "property 'Statistics.Minimum'"
-  ).to.equal(Math.min(...response.json("Throws")));
+  expectBody(response).to.have.property("Statistics");
 
-  expect(response.json("Statistics"), "property 'Statistics'").to.have.property(
-    "Maximum"
+  expectBodyProperty(response, "Statistics").to.have.property("Minimum");
+  expectBodyProperty(response, "Statistics.Minimum").to.equal(
+    Math.min(...response.json("Throws"))
   );
-  expect(
-    response.json("Statistics.Maximum"),
-    "property 'Statistics.Maximum'"
-  ).to.equal(Math.max(...response.json("Throws")));
 
-  expect(response.json("Statistics"), "property 'Statistics'").to.have.property(
-    "Sum"
+  expectBodyProperty(response, "Statistics").to.have.property("Maximum");
+  expectBodyProperty(response, "Statistics.Maximum").to.equal(
+    Math.max(...response.json("Throws"))
   );
-  expect(response.json("Statistics.Sum"), "property 'Statistics.Sum'").to.equal(
+
+  expectBodyProperty(response, "Statistics").to.have.property("Sum");
+  expectBodyProperty(response, "Statistics.Sum").to.equal(
     response.json("Throws").reduce((result, item) => result + item, 0)
   );
 
